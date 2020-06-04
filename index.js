6834f3fabf02e9ff9ef872e541e93a83bda80762
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-04 15:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function saturdayFun(activity="roller-skate") {
 function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective() {
+  
+}
